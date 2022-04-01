1c5f594b0bd24e73d5bfb6bbe0e52ff6bcf86bde
jayshree430 <iyer430@gmail.com>
2022-04-01 14:34:43 +0100

    Extended the CatBehavior class and added a constructor in DomesticCat.java. 2. Some tests pass. Still other tests fail

--- src/main/java/com/techreturners/cats/DomesticCat.java
+++ src/main/java/com/techreturners/cats/DomesticCat.java
@@ -1,31 +1,10 @@
 package com.techreturners.cats;
 
-public class DomesticCat implements Cat {
+public class DomesticCat extends CatBehavior implements Cat {
 
-    @Override
-    public boolean isAsleep() {
-        return false;
-    }
+   public DomesticCat(){
 
-    @Override
-    public void goToSleep() {
-
-    }
-
-    @Override
-    public void wakeUp() {
-
-    }
-
-    @Override
-    public String getSetting() {
-        return null;
-    }
-
-    @Override
-    public int getAverageHeight() {
-        return 0;
-    }
+   }
 
     @Override
     public String eat() {
