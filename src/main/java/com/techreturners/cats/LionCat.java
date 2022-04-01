1ba2950c1a8369db37fbc867141f344f910e3276
jayshree430 <iyer430@gmail.com>
2022-04-01 14:39:21 +0100

    Extended the CatBehavior class and added a constructor in LionCat.java. 2. One test passed. Others failed

--- src/main/java/com/techreturners/cats/LionCat.java
+++ src/main/java/com/techreturners/cats/LionCat.java
@@ -1,34 +1,8 @@
 package com.techreturners.cats;
 
-public class LionCat implements Cat {
+public class LionCat extends CatBehavior implements Cat {
 
-    @Override
-    public boolean isAsleep() {
-        return false;
-    }
-
-    @Override
-    public void goToSleep() {
-
-    }
-
-    @Override
-    public void wakeUp() {
-
-    }
-
-    @Override
-    public String getSetting() {
-        return null;
-    }
-
-    @Override
-    public int getAverageHeight() {
-        return 0;
-    }
-
-    @Override
-    public String eat() {
-        return null;
-    }
+        public LionCat(){
+                averageHeight = 1100;
+        }
 }
